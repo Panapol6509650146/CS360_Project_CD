63028aa8538c1b6f0ea78ede8c04c7f1d1f2f749
Panapol6509650146 <144807621+Panapol6509650146@users.noreply.github.com>
2024-12-06 17:31:03 +0700

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -16,3 +16,12 @@ newgrp docker
 
 sudo docker pull meawrage/petadopt_frontend
 sudo docker run -d -p 3000:3000 --name cs360_frontend_container meawrage/petadopt_frontend:latest
+
+sudo docker pull meawrage/petadopt_backend
+sudo docker run -d -p 1337:1337 --name cs360_backend_container \
+          -e APP_KEYS=${{ secrets.APP_KEYS }} \
+          -e API_TOKEN_SALT=${{ secrets.API_TOKEN_SALT }} \
+          -e ADMIN_JWT_SECRET=${{ secrets.ADMIN_JWT_SECRET }} \
+          -e TRANSFER_TOKEN_SALT=${{ secrets.TRANSFER_TOKEN_SALT }} \
+          -e JWT_SECRET=${{ secrets.JWT_SECRET }} \
+          meawrage/petadopt_backend:latest
