4ec0dc29a9db62cbe4c8b27805722a4c8cc187e1
Panapol6509650146 <144807621+Panapol6509650146@users.noreply.github.com>
2024-12-06 16:14:51 +0700

    Create user-data.sh

---
+++ user-data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd home/ubuntu
+
+sudo apt update
+sudo apt install docker.io -y
+
+sudo service docker start
+sudo systemctl enable docker
+
+sudo usermod -aG docker ubuntu
+newgrp docker
+
+sudo docker pull ${{ secrets.DOCKER_USERNAME }}/petadopt_frontend
+sudo docker run -d -p 3000:3000 --name cs360_frontend_container ${{ secrets.DOCKER_USERNAME }}/petadopt_frontend:latest
